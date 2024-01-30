f0997edf49c1a30b03968bcdb8932d0efaf7eec4
Hanane <elhihi.hanane@yahoo.com>
2024-01-30 16:19:02 +0000

    add a feature that generate a quote randomly

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 
-var q_link = document.getElementById("clickQuote");
 function changePage() {
-  q_link.setAttribute("href", "quote.html");
+
+  $("#quote-button").attr("href", "quote.html");
+  
 }
+
+
---
+++ quote.js
@@ -0,0 +1,5 @@
+var randomNum = Math.floor(Math.random()*18) + 1;
+
+var addr = "media/" + randomNum + ".png" ;
+
+$("#quote-image").attr("src", addr);
