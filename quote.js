a690d6de809c8cf11820a3abbc66877dc387e056
Hanane <88088248+swanhanan@users.noreply.github.com>
2024-01-30 18:53:00 +0000

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,5 +1,5 @@
-var randomNum = Math.floor(Math.random()*18) + 1;
+let randomNum = Math.floor(Math.random()*18) + 1;
 
-var addr = "media/" + randomNum + ".png" ;
+let addr = "media/" + randomNum + ".png" ;
 
 $("#quote-image").attr("src", addr);
