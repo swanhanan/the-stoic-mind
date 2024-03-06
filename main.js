9e653210df463ddeda5568edc7c90b36fea68d95
Hanane <elhihi.hanane@yahoo.com>
2024-03-06 10:21:48 +0000

    merge the js files into main, create public for static files and add footer

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 
-function changePage() {
+const qButton = document.getElementById("quote-button");
 
-  $("#quote-button").attr("href", "quote.html");
+const changePage = () =>{
+
+  qButton.setAttribute("href", "quote.html");
   
-}
+};
 
 
+let randomNum = Math.floor(Math.random()*18) + 1;
+
+let addr = "./media/" + randomNum + ".png" ;
+console.log(addr);
+
+document.getElementById("quote-image").setAttribute("src", addr);
+
+const y_date = document.getElementById("year");
+y_date.innerHTML = new Date().getFullYear();
--- quote.js
+++
@@ -1,5 +0,0 @@
-var randomNum = Math.floor(Math.random()*18) + 1;
-
-var addr = "media/" + randomNum + ".png" ;
-
-$("#quote-image").attr("src", addr);
